0ffaa30b079bad7261e5313a3156e1726de46be4
Kees Postma <kk.postma@gmail.com>
2017-08-18 09:52:19 +0100

    Adds admin boolean field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818082929) do
+ActiveRecord::Schema.define(version: 20170818085033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170818082929) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
   add_foreign_key "admin_profiles", "schools"
