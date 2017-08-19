9bb3757c338627ec569e66ce8eaf7406f062b19c
Kees Postma <kk.postma@gmail.com>
2017-08-19 10:56:18 +0100

    Gets Active Admin to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Active Admin routes
+  ActiveAdmin.routes(self)
+
+  # Root to the homepage
   root 'pages#home'
 
   # Devise
